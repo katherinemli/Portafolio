b180ada6d5d742d418600f179e7406a5c9866ddc
kathymli <kliberona@wherex.com>
2021-02-12 10:28:16 -0300

    kathyestilos

--- src/pages/FolderImg.vue
+++ src/pages/FolderImg.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-page class="index-content">
-    <div style="height: 100hv;  width: 100vh;">
-      <div class="work-style" :style="bodyMargin()">
-        <div class="child child-img">
-            <div class="index-tittle">{{ title }}</div>
-        </div>
+  <q-page
+  v-on:click="scrollToWorkSelelected(urlPicture)"
+  class="index-content">
+    <div v-if="id===1" style="height: 100hv;  width: 100vh;">
+      <div
+      class="work-style" :style="bodyMargin()">
         <div class="child child-2-img" :style="textMargin()">
             <div class="index-top">{{toptext}}</div>
             <div class="index-right">{{subtitle}}</div>
@@ -12,25 +12,62 @@
         </div>
         <div class="child-3-img">
             <div class="content-logo">
-              {{id}}
                 <img
                 src="~assets/hakaton.jpeg"
-                v-if="id===1"
                 id="companyLogo"
                />
+            </div>
+        </div>
+        </div>
+    </div>
+    <div v-if="id===2" style="height: 100hv;  width: 100vh;">
+      <div
+      class="work-style" :style="bodyMargin()">
+        <div class="child child-2-img" :style="textMargin()">
+            <div class="index-top">{{toptext}}</div>
+            <div class="index-right">{{subtitle}}</div>
+            <div class="sub-right">{{subtext}}</div>
+        </div>
+        <div class="child-3-img-right">
+            <div class="content-logo">
                 <img
                 src="~assets/hakaton2.jpg"
-                v-if="id===2"
                 id="companyLogo"
                />
+            </div>
+        </div>
+        </div>
+      </div>
+    <div v-if="id===3" style="height: 100hv;  width: 100vh;">
+      <div
+      class="work-style" :style="bodyMargin()">
+        <div class="child child-2-img" :style="textMargin()">
+            <div class="index-top">{{toptext}}</div>
+            <div class="index-right">{{subtitle}}</div>
+            <div class="sub-right">{{subtext}}</div>
+        </div>
+        <div class="child-3-img">
+            <div class="content-logo">
                 <img
                 src="~assets/sube1.jpg"
-                v-if="id===3"
                 id="companyLogo"
                />
+            </div>
+        </div>
+        </div>
+      </div>
+    <div v-if="id===4" style="height: 100hv;  width: 100vh;">
+      <div
+      class="work-style" :style="bodyMargin()">
+        <div class="child child-2-img" :style="textMargin()">
+            <div class="index-top">{{toptext}}</div>
+            <div class="index-right">{{subtitle}}</div>
+            <div class="sub-right">{{subtext}}</div>
+        </div>
+        <div class="child-3-img-right">
+            <div class="content-logo">
                 <img
                 src="~assets/sube2.jpg"
-                v-if="id===4"
                 id="companyLogo"
                />
             </div>
@@ -100,6 +137,11 @@ export default {
       console.log('entro?1: ', this.marginTop);
       console.log('entro?2: ', this.marginLeft);
       return `@/assets/${this.urlPicture}`;
+    },
+    scrollToWorkSelelected(url) {
+      console.log('que se esta mandando?', url);
+      const urlComplete = url.trim('/');
+      this.$router.push({ path: url, params: { id: urlComplete } });
     },
   },
 };
--- src/pages/Works.vue
+++ src/pages/Works.vue
@@ -3,41 +3,41 @@
     <FolderImg
     v-bind:marginTop="marginTop"
     v-bind:marginLeft="marginLeft"
-    v-bind:subtitle="subtitle"
-    v-bind:toptext="toptext"
-    v-bind:subtext="subtext"
+    v-bind:subtitle="users[0].name"
+    v-bind:toptext="users[0].company"
+    v-bind:subtext="users[0].about"
     v-bind:marginLeftTxt="marginLeftTxt"
-    v-bind:urlPicture="urlPicture"
+    v-bind:urlPicture="users[0].url"
     v-bind:id="1"
     />
     <FolderImg
-    v-bind:marginTop="marginTop"
-    v-bind:marginLeft="marginLeft"
-    v-bind:subtitle="subtitle"
-    v-bind:toptext="toptext"
-    v-bind:subtext="subtext"
-    v-bind:marginLeftTxt="marginLeftTxt"
-    v-bind:urlPicture="urlPicture2"
+    v-bind:marginTop="'-4vw'"
+    v-bind:marginLeft="'58.6vw'"
+    v-bind:subtitle="users[1].name"
+    v-bind:toptext="users[1].company"
+    v-bind:subtext="users[1].about"
+    v-bind:marginLeftTxt="'-51.4vw'"
+    v-bind:urlPicture="users[1].url"
     v-bind:id="2"
     />
     <FolderImg
     v-bind:marginTop="marginTop"
     v-bind:marginLeft="marginLeft"
-    v-bind:subtitle="subtitle"
-    v-bind:toptext="toptext"
-    v-bind:subtext="subtext"
+    v-bind:subtitle="users[2].name"
+    v-bind:toptext="users[2].company"
+    v-bind:subtext="users[2].about"
     v-bind:marginLeftTxt="marginLeftTxt"
-    v-bind:urlPicture="urlPicture3"
+    v-bind:urlPicture="users[2].url"
     v-bind:id="3"
     />
     <FolderImg
-    v-bind:marginTop="marginTop"
-    v-bind:marginLeft="marginLeft"
-    v-bind:subtitle="subtitle"
-    v-bind:toptext="toptext"
-    v-bind:subtext="subtext"
-    v-bind:marginLeftTxt="marginLeftTxt"
-    v-bind:urlPicture="urlPicture4"
+    v-bind:marginTop="'-4vw'"
+    v-bind:marginLeft="'58.6vw'"
+    v-bind:subtitle="users[3].name"
+    v-bind:toptext="users[3].company"
+    v-bind:subtext="users[3].about"
+    v-bind:marginLeftTxt="'-51.4vw'"
+    v-bind:urlPicture="users[3].url"
     v-bind:id="4"
 
     />
@@ -87,12 +87,13 @@ export default {
       users: usersData,
       heightValue: 700,
       widthValue: 800,
-      marginTop: '2vh',
+
       urlPicture: 'hakaton.jpeg',
       urlPicture2: 'hakaton2.jpg',
       urlPicture3: 'sube1.jpg',
       urlPicture4: 'sube2.jpg',
-      marginLeft: '-28vw',
+      marginTop: '2vh',
+      marginLeft: '-5vw',
       marginLeftTxt: '50.6vw',
       selectedCursor: `url("data:image/svg+xml,%3Csvg version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='32px' height='32px' viewBox='0 0 512 512' style='enable-background:new 0 0 512 512;' xml:space='preserve'%3E %3Cpath d='M443.6,387.1L312.4,255.4l131.5-130c5.4-5.4,5.4-14.2,0-19.6l-37.4-37.6c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4 L256,197.8L124.9,68.3c-2.6-2.6-6.1-4-9.8-4c-3.7,0-7.2,1.5-9.8,4L68,105.9c-5.4,5.4-5.4,14.2,0,19.6l131.5,130L68.4,387.1 c-2.6,2.6-4.1,6.1-4.1,9.8c0,3.7,1.4,7.2,4.1,9.8l37.4,37.6c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1L256,313.1l130.7,131.1 c2.7,2.7,6.2,4.1,9.8,4.1c3.5,0,7.1-1.3,9.8-4.1l37.4-37.6c2.6-2.6,4.1-6.1,4.1-9.8C447.7,393.2,446.2,389.7,443.6,387.1z'/%3E %3C/svg%3E"), pointer`,
     };
